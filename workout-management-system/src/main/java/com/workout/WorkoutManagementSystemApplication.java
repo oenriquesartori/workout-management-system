b203fc432468879889d9634d8f51d41353f4d508
oenriquesartori <enriquesartori06@gmail.com>
2025-04-24 16:47:17 -0300

    arrumando as paradas

---
+++ workout-management-system/src/main/java/com/workout/WorkoutManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.workout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutManagementSystemApplication.class, args);
+	}
+
+}
