b0195219eac0aab216bce9e91ebbc90657250140
Eduardo Sartori <edusartori00@gmail.com>
2025-04-25 15:31:17 -0300

    feat: add anotations in user entity

--- src/main/java/com/workout/entity/User.java
+++ src/main/java/com/workout/entity/User.java
@@ -1,7 +1,6 @@
 package com.workout.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -14,13 +13,16 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @Builder
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private LocalDate birthDate;
+    @Enumerated(EnumType.STRING)
     private Gender gender;
     private Double height;
     private Double weight;
     private LocalDate startDate;
     private String objective;
-
 }
