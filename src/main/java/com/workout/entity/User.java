bcaa2c180b2faf598d4ee8d07e0db0726e279e9b
oenriquesartori <enriquesartori06@gmail.com>
2025-04-25 15:16:51 -0300

    cria~ção dos atributos do usuário

--- src/main/java/com/workout/entity/User.java
+++ src/main/java/com/workout/entity/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.*;
 
+import java.time.LocalDate;
+
 @Entity
 @Table(name = "users")
 @Getter
@@ -12,4 +14,13 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class User {
+    private Long id;
+    private String name;
+    private LocalDate birthDate;
+    private Gender gender;
+    private Double height;
+    private Double weight;
+    private LocalDate startDate;
+    private String objective;
+
 }
